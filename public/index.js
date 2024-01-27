30409b5bec07f2b2d87fc6365ec9a2253b066e0b
twarped <samthebeastofall>
2024-01-27 12:49:58 -0700

    Added some stuff

---
+++ public/index.js
@@ -0,0 +1,11 @@
+const navA = document.body.querySelectorAll('nav a');
+
+navA.forEach(el => {
+    el.addEventListener('click', el => {
+        navA.forEach(el => {
+            el.classList.remove('active');
+        });
+        el.target.classList.add('active');
+    });
+});
+
